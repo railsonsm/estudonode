bbe41fc014ba8858b892e60e21140a6e72e87bb0
Railson <railson170@hotmail.com>
2018-08-01 22:32:14 -0300

    Adicionado consign para carregamento automatico comencando a trabalhar com models

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,13 +1,16 @@
-var connection = require('../../config/dbConnection')();
+//var connection = require('../../config/dbConnection')();
 
 module.exports = function(app){
+   
     app.get('/noticias', function(req, res){
-        
-        connection.query("select * from noticias", function(erro, result){
-            console.log(result);
-            
+        var connection = app.config.dbConnection();
+        var noticiasModel = app.app.models.noticiasModel;
+
+        noticiasModel.getNoticias(connection, function(erro, result){          
             res.render("noticias/noticias", {noticias : result});
         });
+
+      
         //res.render("noticias/noticias");
     });
 }
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,14 @@
 var mysql = require('mysql');
-
-module.exports = function(){
+var connMysql = function(){
+    console.log('Conexao com o banco foi establecida');
     return  mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '123456',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function(){
+    return connMysql;  
 }
